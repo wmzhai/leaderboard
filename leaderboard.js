4fdcb2bd5a5854ea8e508c5bb3aeca124fd86326
Weiming Zhai <wmzhai@gmail.com>
2015-04-08 16:55:22 +0800

    before account

--- leaderboard.js
+++ leaderboard.js
@@ -1,23 +1,64 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+PlayersList = new Mongo.Collection('players');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if( Meteor.isClient){
+  console.log("Hello Client");
+
+  Template.leaderboard.helpers({
+    'player' : function(){
+
+      return PlayersList.find({},{sort:{score:-1}});
+
+    },
+    'selectedClass': function () {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if( playerId == selectedPlayer){
+        return "selected";
+      }
+    },
+    'showSelectedPlayer' : function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
+    }
+
+  });
+
+  Template.leaderboard.events({
+    'click .player' : function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+    'click .increment' : function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer);
+
+      PlayersList.update(selectedPlayer,{$inc :{score:5}});
+    },
+    'click .decrement' : function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer,{$inc:{score:-5}});
+    },
+    'click .remove': function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.remove(selectedPlayer);
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+
+
+  Template.addPlayerForm.events({
+    'submit form' : function (event) {
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      console.log(playerNameVar);
+      PlayersList.insert({
+        name:playerNameVar,
+        score:0
+      });
+
     }
   });
 }
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+else{
+  console.log("Hello server");
 }
